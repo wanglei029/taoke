15c91faf120e25c8c380a298e0e6457a5c16dfa2
wanglei029 <610213949@qq.com>
2020-06-03 10:52:41 +0800

    无用的操作

---
+++ src/api/axios.js
@@ -0,0 +1,5 @@
+import originAxios from "axios"
+
+export default function axios(url){
+    originAxios(url)
+}
---
+++ src/api/common.js
@@ -0,0 +1,5 @@
+import originAxios from "axios"
+export function getTabIcon(){
+    const url="http://cmsjapi.ffquan.cn/api/category/product/model-detail-by-model-id-new?entityId=3&modelId=1&proModelId=1&source=3&version=v1&tuserId=1206917&isWechat=0";
+
+}
